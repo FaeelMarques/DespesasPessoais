aa9b584db1b1b58cbcadbdc4419e8a3ecdc473d7
Rafael Taranto <faeel.marques@gmail.com>
2022-10-09 13:57:48 -0300

    inicio do projeto, com função de criar no estágio inicial

---
+++ expenses/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:expenses/components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: const Card(
+              child: Text('Gráfico'),
+            ),
+          ),
+          const TransactionUser(),
+        ],
+      ),
+    );
+  }
+}
